fa8dd3443c1267b3604eed1f764b91f682000964
AlagarPrabu <85381546+AlagarPrabu@users.noreply.github.com>
2021-06-22 06:59:26 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import asai_conversion
+
+def get_input():
+  get_string = input("Enter your string: ")
+  if(get_string == "exit"):
+    exit()
+  else:
+    get_asai = asai_conversion.word_convert(get_string)
+    print()
+    print("Given string asai is : " +str(get_asai))
+    print()
+    get_input()
+    
+if __name__ == "__main__":
+  get_input()
